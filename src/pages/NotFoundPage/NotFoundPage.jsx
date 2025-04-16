633d4f21aa853f7fc8217f6c464b277b85243e97
myroslava <murosja80@gmail.com>
2025-04-16 23:01:44 +0300

    hw_05

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
-    <main className={styles.container}>
-      <h2 className={styles.title}>Page is not found...</h2>
+    <main>
+      <h2>Page is not found...</h2>
     </main>
   );
 };
